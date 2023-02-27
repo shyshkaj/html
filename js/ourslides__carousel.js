13bef628af8814e4f09dd02be069f8ffffd6ea15
shyshka <83885376+shyshkaj@users.noreply.github.com>
2023-02-27 14:28:01 +0000

    Gfgfg

--- js/ourslides__carousel.js
+++ js/ourslides__carousel.js
@@ -1,23 +1,19 @@
 (function () {
 
-    const slides = [
-
+    const slides  = [
+        '<div><img src="img/blackoffice.png" alt="black office"></div>',
+        '<div><img src="img/brownoffice.png" alt="broenoffice"></div>',
+        '<div><img src="img/privateoffice.png" alt="privateoffice"></div>',
+        '<div><img src="img/singleoffice.png" alt="singleoffice"></div>',
     ];
 
     let currentSlide = 0;
 
     function renderSlides(slides) {
-        const slidesContainer = document.querySelector('.products-carousel__slides');
+        const slidesContainer = document.querySelector('.ourspaces-carousel__slides');
         slidesContainer.innerHTML = slides[currentSlide];
-        if (window.innerWidth > 768) {
-            const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-            slidesContainer.innerHTML += slides[secondSlide];
-            if (window.innerWidth > 900) {
-                const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
-                slidesContainer.innerHTML += slides[thirdSlide];
-            }
         }
-    }
+    
 
     function nextSlide() {
         currentSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
@@ -29,16 +25,15 @@
         renderSlides(slides);
     }
 
-    setInterval(nextSlide, 3000);
-
     renderSlides(slides);
 
-    const nextButton = document.querySelector('.products-carousel__next');
+    const nextButton = document.querySelector('.carousel__button--left');
     nextButton.addEventListener('click', nextSlide);
 
-    const prevButton = document.querySelector('.products-carousel__prev');
+    const prevButton = document.querySelector('.carousel__button--right');
     prevButton.addEventListener('click', prevSlide);
 
     window.addEventListener('resize', () => {
         renderSlides(slides);
     });
+})();
